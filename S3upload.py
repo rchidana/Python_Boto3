47ca1e69dc66323b5577ea4ac176d6d5e78b2b35
Chid R <anandr72@gmail.com>
2018-07-08 19:29:25 +0530

    Update S3upload.py

--- S3upload.py
+++ S3upload.py
@@ -4,11 +4,13 @@ import boto3
 s3 = boto3.client('s3')
 
 filename = 'file2.txt'
-bucket_name = 'chid-bucket'
+bucket_name = 'chid-bucket22'
 
 # Uploads the given file using a managed uploader, which will split up large
 # files automatically and upload parts in parallel.
 
-
-
-s3.upload_file(filename, bucket_name, filename)
+try:
+	response = s3.upload_file(filename, bucket_name, filename)
+	print (response)
+except Exception as error:
+	print (error)
