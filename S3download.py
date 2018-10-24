45086a852fde43a0819cc2cea26399b5799de760
Anand R <anandr72@gmail.com>
2018-10-24 10:35:57 +0530

    Error messages corrected

--- S3download.py
+++ S3download.py
@@ -2,14 +2,15 @@ import boto3
 import botocore
 
 BUCKET_NAME = 'chid-bucket' # replace with your bucket name
-KEY = 'Projects.jpg' # replace with your object key
+FILE_NAME= 'file.txt' # replace with the your specific file that exists in your bucket
 
 s3 = boto3.resource('s3')
 
 try:
-    s3.Bucket(BUCKET_NAME).download_file(KEY, 'my_local_image.jpg')
+    s3.Bucket(BUCKET_NAME).download_file(FILE_NAME,FILE_NAME)
+    print("Downloaded File : ",FILE_NAME," successfully from Bucket : ",BUCKET_NAME)
 except botocore.exceptions.ClientError as e:
     if e.response['Error']['Code'] == "404":
-        print("The object does not exist.")
+        print("The File ",FILE_NAME," does not exist in Bucket : ",BUCKET_NAME)
     else:
         raise
--- S3upload.py
+++ S3upload.py
@@ -3,14 +3,14 @@ import boto3
 # Create an S3 client
 s3 = boto3.client('s3')
 
-filename = 'file2.txt'
-bucket_name = 'chid-bucket22'
+filename = 'S3.py'
+bucket_name = 'chid-bucket'
 
 # Uploads the given file using a managed uploader, which will split up large
 # files automatically and upload parts in parallel.
 
 try:
 	response = s3.upload_file(filename, bucket_name, filename)
-	print (response)
+	print ("Uploaded File ",filename," Successfully on to Bucket : ",bucket_name)
 except Exception as error:
 	print (error)
